807de14cf8fc726c0e257fd2fe8705832923b621
KevinPMyers <kmyers25@gmail.com>
2021-10-21 18:44:46 -0400

    oneDrive crash, push to save any progress

---
+++ server/models/Date.js
@@ -0,0 +1,36 @@
+const { Schema, model } = require('mongoose');
+// const recipeSchema = require('./Recipe');
+// const musicSchema = require('./Music')
+
+const dateSchema = new Schema({
+    datename: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now
+    },
+    
+    username: [{
+        type: Schema.Types.ObjectId,
+        ref: 'User'
+    }],
+    recipes: [{
+        type: Schema.Types.ObjectId,
+        ref: 'Recipe'
+    }],
+    // or would this be better
+    // recipes: [recipeSchema]
+    music: [{
+        type: Schema.Types.ObjectId,
+        ref: 'Music'
+    }]
+    // music: [musicSchema]
+    
+});
+
+const Date = model('Date', dateSchema);
+
+module.exports = Date;
---
+++ server/models/index.js
@@ -0,0 +1,4 @@
+const User = require('./User');
+const Date = require('./Date');
+
+module.exports = { User, Date };
