dd292e2834a8d6d87b97f12409c7313e4775339c
Logan D <logandufek@gmail.com>
2021-10-25 23:01:46 -0400

    added minor styling changes and carousel to landing page with GIFs

---
+++ client/src/components/Carousel/index.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import { Carousel, Button } from 'antd';
+import CarouselOne from '../../assets/Carousel1.gif'
+import CarouselTwo from '../../assets/Carousel2.gif'
+
+
+function FoodCarousel() {
+
+    const contentStyle = {
+        height: '700px',
+        color: '#fff',
+        lineHeight: '160px',
+        textAlign: 'center',
+
+    };
+
+
+    return (
+        <div>
+            <Carousel autoplay>
+                <div>
+                    <img className="carousel" src={CarouselOne}></img>
+
+                </div>
+                <div>
+                    <img className="carousel" src={CarouselTwo}></img>
+                </div>
+
+
+            </Carousel>,
+
+        </div>
+    )
+}
+
+export default FoodCarousel
